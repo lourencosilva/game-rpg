d5b332979b0674f8bf0d44149a4bb8cb2c86b975
Lourenço Silva Júnior <lourenco.silva72@gmail.com>
2021-03-22 07:15:42 -0300

    Colocando metodos no lugar certo

--- raca.rb
+++ raca.rb
@@ -20,29 +20,29 @@ class Raca
   end  
 end
 
-def racas_default()
-$racas = []
-$raca = Raca.new("Humano", 20, 8, 8, 10, 10)
-$racas << $raca
+# def racas_default()
+# $racas = []
+# $raca = Raca.new("Humano", 20, 8, 8, 10, 10)
+# $racas << $raca
 
-$raca = Raca.new("Elfo", 25,5, 6, 20, 10)
-$racas << $raca
+# $raca = Raca.new("Elfo", 25,5, 6, 20, 10)
+# $racas << $raca
 
-$raca = Raca.new("Anao", 18, 9, 11, 7, 20)
-$racas << $raca
+# $raca = Raca.new("Anao", 18, 9, 11, 7, 20)
+# $racas << $raca
 
-$raca = Raca.new("Orc", 15, 12, 5, 2, 25)
-$racas << $raca
-end
+# $raca = Raca.new("Orc", 15, 12, 5, 2, 25)
+# $racas << $raca
+# end
 
-def listar_racas
-puts '╔═══════════════════════════════════════════════╗'
-puts '║               RACAS  CADASTRADAS              ║'
-puts '╚═══════════════════════════════════════════════╝'
-# byebug
-cont = 1
-$racas.each do |valor|
-  puts "   ► #{cont} - #{valor}"
-  cont += 1
-end
-end
+# def listar_racas
+# puts '╔═══════════════════════════════════════════════╗'
+# puts '║               RACAS  CADASTRADAS              ║'
+# puts '╚═══════════════════════════════════════════════╝'
+# # byebug
+# cont = 1
+# $racas.each do |valor|
+#   puts "   ► #{cont} - #{valor}"
+#   cont += 1
+# end
+# end
